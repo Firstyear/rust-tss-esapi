973ef0b6c7303396fab7fcb86d994df5271acf0d
William <william@firstyear.id.au>
2026-08-01 14:59:16 +1000

    Test create loaded in a different context

--- tss-esapi/tests/integration_tests/context_tests/tpm_commands/object_commands_tests.rs
+++ tss-esapi/tests/integration_tests/context_tests/tpm_commands/object_commands_tests.rs
@@ -466,6 +466,7 @@ mod test_create_loaded {
             .build()
             .expect("Failed to build derive parent public");
 
+        /*
         let create_result = context
             .create(
                 primary_key_handle,
@@ -486,12 +487,12 @@ mod test_create_loaded {
         let derive_parent_handle = context
             .load(primary_key_handle, out_private, out_public)
             .expect("Failed to load derivation parent.");
+        */
 
-        /*
         // We should be able to create and load this now. And look, like magic,
         // it's created and loaded in one operation!
         let create_loaded_result = context
-            .create_loaded(primary_key_handle, None, None, derive_parent_public)
+            .create_loaded(primary_key_handle, derive_parent_public, None, None)
             .expect("Failed to create derivation parent.");
 
         let CreateLoadedKeyResult {
@@ -499,7 +500,6 @@ mod test_create_loaded {
             out_private: derive_parent_private,
             out_public: derive_parent_public,
         } = create_loaded_result;
-        */
 
         context
             .flush_context(primary_key_handle.into())
